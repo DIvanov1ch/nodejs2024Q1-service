441c67c019e081a1702d8560d6433e4ce83eb134
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-03-08 17:07:18 +0300

    feat: implement main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { serve, setup } from 'swagger-ui-express';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  DEFAULT_PORT,
+  PATH_TO_YAML_FOLDER,
+  YAML_SWAGGER_FILENAME,
+} from './constants';
+import { load } from 'js-yaml';
+import { join } from 'node:path';
+import { readFileSync } from 'node:fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerDocument = load(
+    readFileSync(join(PATH_TO_YAML_FOLDER, YAML_SWAGGER_FILENAME), 'utf8'),
+  );
+  app.use('/doc', serve, setup(swaggerDocument));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || DEFAULT_PORT;
+  await app.listen(port);
+  console.log(`Server listening at http://localhost:${port}`);
 }
 bootstrap();
