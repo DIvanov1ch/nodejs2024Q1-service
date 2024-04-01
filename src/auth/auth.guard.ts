54c6cf8ba2e1f3d3b461f68e920f52252f5d634e
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2024-04-01 17:56:05 +0300

    feat: implement auth module

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,63 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { REFRESH_ROUTE } from 'src/constants/constants';
+import { IS_PUBLIC_KEY } from 'src/public/public.decorator';
+import { RefreshDto } from './dto/refresh.dto';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.isPublicRoute(context);
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.getTokenFromRequest(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    if (request.originalUrl === REFRESH_ROUTE) {
+      return true;
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  private isPublicRoute(context: ExecutionContext): boolean {
+    return this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+  }
+
+  private getTokenFromRequest(request: Request) {
+    const { originalUrl, body } = request;
+
+    return originalUrl === REFRESH_ROUTE
+      ? (body as RefreshDto).refreshToken
+      : this.extractTokenFromHeader(request);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import { Body, Injectable, ForbiddenException } from '@nestjs/common';
+import { Messages } from 'src/constants/constants';
+import { UserService } from 'src/user/user.service';
+import { LogInDto } from './dto/log-in.dto';
+import { RefreshDto } from './dto/refresh.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import { JwtService } from '@nestjs/jwt';
+import { isEqualPassword } from 'src/utils/hash-password.utils';
+import { Payload, Tokens } from './types/auth.types';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(@Body() signUpDto: SignUpDto): Promise<any> {
+    return await this.usersService.create(signUpDto);
+  }
+
+  async logIn(@Body() logInDto: LogInDto): Promise<Tokens> {
+    const { login, password } = logInDto;
+    const user = await this.usersService.findFirst(login);
+
+    const isEqual = await isEqualPassword(password, user.password);
+    if (!isEqual) {
+      throw new ForbiddenException(Messages.INCORRECT_PASSWORD);
+    }
+
+    return await this.getTokens({ userId: user.id, login: user.login });
+  }
+
+  async refresh(@Body() refreshDto: RefreshDto): Promise<Tokens> {
+    try {
+      const { userId, login } = (await this.jwtService.verifyAsync(
+        refreshDto.refreshToken,
+        {
+          secret: process.env.JWT_SECRET_REFRESH_KEY,
+        },
+      )) as Payload;
+      return await this.getTokens({ userId, login });
+    } catch (error) {
+      throw new ForbiddenException(Messages.INVALID_REFRESH_TOKEN);
+    }
+  }
+
+  async getTokens(payload: Payload): Promise<Tokens> {
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+      refreshToken: await this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_SECRET_REFRESH_KEY,
+        expiresIn: process.env.TOKEN_REFRESH_EXPIRE_TIME,
+      }),
+    };
+  }
+}
